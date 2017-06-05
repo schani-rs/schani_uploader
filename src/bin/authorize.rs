47c988d910cffc4fb4db348729726572784c6ac0
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-06-05 19:01:21 +0200

    Split autorization and upload code
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

---
+++ src/bin/authorize.rs
@@ -0,0 +1,18 @@
+extern crate schani_uploader;
+extern crate dotenv;
+
+use std::env;
+use dotenv::dotenv;
+use schani_uploader::common::Platform;
+use schani_uploader::fivehundredpx::Client;
+
+pub fn main() {
+    dotenv().ok();
+
+    let x500px_key = env::var("X500PX_KEY").expect("X500PX_KEY must be set");
+    let x500px_secret = env::var("X500PX_SECRET").expect("X500PX_SECRET must be set");
+
+    let platform = Client::new(x500px_key, x500px_secret);
+
+    platform.autorize();
+}
--- src/fivehundredpx/client.rs
+++ src/fivehundredpx/client.rs
@@ -1,6 +1,7 @@
 use hyper::Client as HyperClient;
 use hyper::net::HttpsConnector;
 use hyper_openssl::OpensslClient;
+use std::io;
 use super::oauth::*;
 use super::upload::upload_image;
 use super::super::error::UploadError;
@@ -9,13 +10,18 @@ use super::super::common::{Platform, Photo};
 
 #[derive(Debug)]
 pub struct Client {
+    client: HyperClient,
     consumer_key: String,
     consumer_secret: String,
 }
 
 impl Client {
     pub fn new(key: String, secret: String) -> Client {
+        let ssl = OpensslClient::new().unwrap();
+        let connector = HttpsConnector::new(ssl);
+        let client = HyperClient::with_connector(connector);
         Client {
+            client: client,
             consumer_key: key,
             consumer_secret: secret,
         }
@@ -24,14 +30,27 @@ impl Client {
 
 impl Platform for Client {
     fn authorize(&self) {
-        let ssl = OpensslClient::new().unwrap();
-        let connector = HttpsConnector::new(ssl);
-        let client = HyperClient::with_connector(connector);
-        get_oauth_access_token(&client, &self.consumer_key, &self.consumer_secret)
-            .expect("could not get an OAuth access token")
+        let (request_token, authorize_url) =
+            authorize(&self.client, &self.consumer_key, &self.consumer_secret)
+                .expect("could not authorize");
+
+        println!("Please visit {} and grant access to your account.",
+                 authorize_url);
+
+        let mut verifier = String::new();
+        io::stdin()
+            .read_line(&mut verifier)
+            .expect("Failed to read line");
+
+        get_oauth_access_token(&self.client,
+                               &self.consumer_key,
+                               &self.consumer_secret,
+                               &request_token,
+                               &verifier)
+                .expect("could not get access token");
     }
 
-    fn upload(&self, photo: &Photo) -> Result<(), UploadError>  {
+    fn upload(&self, photo: &Photo) -> Result<(), UploadError> {
         let oauth_token = "xxx".to_string();
         let oauth_token_secret = "yyy".to_string();
         upload_image(&self.consumer_key,
