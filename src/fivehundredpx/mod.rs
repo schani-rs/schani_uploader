110817cb4a78b7af2e74dff215c02afc15cf0cad
Christoph Wurst <christoph@winzerhof-wurst.at>
2017-06-05 11:57:25 +0200

    Refactor oauth related code into a module of its own
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- src/fivehundredpx/mod.rs
+++ src/fivehundredpx/mod.rs
@@ -1,3 +1,5 @@
 pub mod client;
+mod oauth;
+mod oauth_header;
 
 pub use self::client::Client;
---
+++ src/fivehundredpx/oauth.rs
@@ -0,0 +1,111 @@
+use hyper::Client;
+use hyper::method::Method::Post;
+use hyper::header::Authorization;
+use oauthcli::*;
+use std::borrow::Cow;
+use url::Url;
+use url::form_urlencoded;
+use std::collections::HashMap;
+use std::io;
+use std::io::Read;
+use super::oauth_header::OAuthHeader;
+
+#[derive(Debug)]
+struct OAuthRequestToken<'a> {
+    value: Cow<'a, str>,
+    secret: Cow<'a, str>,
+}
+
+fn get_oauth_request_token<'a>(client: &Client,
+                               consumer_key: &'a String,
+                               consumer_secret: &'a String)
+                               -> Result<OAuthRequestToken<'a>, String> {
+    let url = Url::parse("https://api.500px.com/v1/oauth/request_token").unwrap();
+    let oauth_header = OAuthAuthorizationHeaderBuilder::new("POST",
+                                                            &url,
+                                                            consumer_key.to_string(),
+                                                            consumer_secret.to_string(),
+                                                            SignatureMethod::HmacSha1)
+            .finish()
+            .to_string();
+    let hdr = OAuthHeader::new(oauth_header);
+
+    let mut resp = client
+        .request(Post, url)
+        .header(Authorization(hdr))
+        .send()
+        .unwrap();
+
+    let mut body = vec![];
+    resp.read_to_end(&mut body).unwrap();
+    let resp_body = String::from_utf8_lossy(&body).into_owned();
+
+    let parsed = form_urlencoded::parse(resp_body.as_bytes());
+    let mut hash_query: HashMap<String, String> = parsed.into_owned().collect();
+    assert!(hash_query.contains_key("oauth_token_secret"));
+    assert!(hash_query.contains_key("oauth_token"));
+    let req_token: String = hash_query.remove("oauth_token").expect("");
+    let req_token_secret: String = hash_query.remove("oauth_token_secret").expect("");
+    Ok(OAuthRequestToken {
+           value: req_token.into(),
+           secret: req_token_secret.into(),
+       })
+}
+
+fn authorize<'a>(client: &Client,
+                 consumer_key: &'a String,
+                 consumer_secret: &'a String)
+                 -> Result<(OAuthRequestToken<'a>, String), String> {
+    let request_token = get_oauth_request_token(client, consumer_key, consumer_secret)?;
+
+    let mut authorize_url = Url::parse("https://api.500px.com/v1/oauth/authorize").unwrap();
+    authorize_url
+        .query_pairs_mut()
+        .append_pair("oauth_token", &request_token.value);
+    println!("Please visit {} and grant access to your account.",
+             authorize_url);
+
+    let mut verifier = String::new();
+
+    io::stdin()
+        .read_line(&mut verifier)
+        .expect("Failed to read line");
+
+    Ok((request_token, verifier))
+}
+
+pub fn get_oauth_access_token<'a>(client: &Client,
+                                  consumer_key: &'a String,
+                                  consumer_secret: &'a String)
+                                  -> Result<(), String> {
+    let (request_token, verifier) = authorize(client, consumer_key, consumer_secret)?;
+
+    println!("Got verifier {}", verifier);
+
+    let url = Url::parse("https://api.500px.com/v1/oauth/access_token").unwrap();
+    let oauth_header = OAuthAuthorizationHeaderBuilder::new("POST",
+                                                            &url,
+                                                            consumer_key.to_string(),
+                                                            consumer_secret.to_string(),
+                                                            SignatureMethod::HmacSha1)
+            .token(request_token.value, request_token.secret)
+            .verifier(verifier.trim())
+            .finish()
+            .to_string();
+    let hdr = OAuthHeader::new(oauth_header);
+
+    let mut resp = client
+        .request(Post, url)
+        .header(Authorization(hdr))
+        .send()
+        .unwrap();
+
+    let mut body = vec![];
+    resp.read_to_end(&mut body).unwrap();
+    let resp_body = String::from_utf8_lossy(&body).into_owned();
+
+    println!("Status: {}", resp.status);
+    println!("Body: {}", resp_body);
+
+    Ok(())
+}
